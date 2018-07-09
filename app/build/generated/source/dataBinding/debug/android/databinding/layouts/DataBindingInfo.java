e064efc74f585ff42dd4decf388ee4e7593a74a1
KatonaGyorgy <gy.katona7@gmail.com>
2018-07-09 14:45:37 +0200

    second commmit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="27543370-1628-4c9d-88f7-5b6aac4fa32b")
+public class DataBindingInfo {}
